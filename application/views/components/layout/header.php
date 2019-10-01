a79d43aaaa240873f35ed3259da99654b75627ff
João Victor <victorvilela00@gmail.com>
2019-10-01 12:59:40 -0300

    Montagem de header

--- application/views/components/layout/header.php
+++ application/views/components/layout/header.php
@@ -1,9 +1,66 @@
-<nav class="navigation-primary navbar navbar-expand-lg <?= !empty($class) ? $class : '' ?>">
-    <div class="container">
-        <a class="navbar-brand mr-auto" href="<?= $this->routeUrl('home') ?>" title="<?= $this->config('app', 'name') ?>">
-            <img class="navigation-primary__logo" src="<?= $this->url('/assets/img/logo-mix-dark.svg') ?>" alt="<?= $this->config('app', 'name') ?>">
-        </a>
-
-        <?= \View::factory('components/layout/header/navigation') ?>
-    </div>
-</nav>
+<header class="header-site">
+	<a href="javascript:;" class="logo">
+		<img src="<?= $this->url('/assets/img/logo.png') ?>" alt="">
+	</a>
+	<div class="nav-site">
+		<div>
+			<div class="nav-top">
+				<div class="box-language">
+					<ul>
+						<li>
+							<a href="javascript:;"><img src="<?= $this->url('/assets/img/eua.png') ?>" alt=""></a>
+						</li>
+						<li>
+							<a href="javascript:;"><img src="<?= $this->url('/assets/img/spanish.png') ?>" alt=""></a>
+						</li>
+					</ul>
+				</div>
+				<div class="box-network">
+					<ul>
+						<li>
+							<a href="javascript:;"><img src="<?= $this->url('/assets/img/icon-instagram.png') ?>" alt=""></a>
+						</li>
+						<li>
+							<a href="javascript:;"><img src="<?= $this->url('/assets/img/icon-instagram.png') ?>" alt=""></a>
+						</li>
+						<li>
+							<a href="javascript:;"><img src="<?= $this->url('/assets/img/icon-instagram.png') ?>" alt=""></a>
+						</li>
+						<li>
+							<a href="javascript:;"><img src="<?= $this->url('/assets/img/icon-instagram.png') ?>" alt=""></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="diviser-nav"></div>
+			<div class="nav-bottom">
+				<nav class="nav-menu">
+					<ul>
+						<li>
+							<a href="javascript:;">A casa</a>
+						</li>
+						<li>
+							<a href="javascript:;">Diagnóstico precoce</a>
+						</li>
+						<li>
+							<a href="javascript:;">Projetos</a>
+						</li>
+						<li>
+							<a href="javascript:;">Colabore</a>
+						</li>
+						<li>
+							<a href="javascript:;">Mídia</a>
+						</li>
+						<li>
+							<a href="javascript:;">Parceiros</a>
+						</li>
+						<li>
+							<a href="javascript:;">Fale conosco</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+		<a href="javascript:;" class="btn-doe"><img src="<?= $this->url('/assets/img/heart-yellow.svg') ?>" alt="">Doe agora</a>
+	</div>
+</header>
