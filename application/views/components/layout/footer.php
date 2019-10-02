b6f7489dc88782e2116f215ba8a466f9d05262f8
João Victor <victorvilela00@gmail.com>
2019-10-02 12:27:40 -0300

    Footer

--- application/views/components/layout/footer.php
+++ application/views/components/layout/footer.php
@@ -1,5 +1,87 @@
-<footer class="footer">
-
+<footer class="footer-site">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-4 col-lg-2 d-none d-md-block">
+                <ul class="menu-footer">
+                    <li class="title-list">A casa</li>
+                    <li><a href="javascript:;">História</a></li>
+                    <li><a href="javascript:;">Casa</a></li>
+                    <li><a href="javascript:;">Missão</a></li>
+                    <li><a href="javascript:;">Diretoria</a></li>
+                    <li><a href="javascript:;">Setores</a></li>
+                    <li><a href="javascript:;">Equipe</a></li>
+                    <li><a href="javascript:;">Depoimentos</a></li>
+                    <li><a href="javascript:;">Resultados</a></li>
+                    <li><a href="javascript:;">Prêmios</a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-sm-4 col-lg-2 d-none d-md-block">
+                <ul class="menu-footer">
+                    <li class="title-list">Colabore</li>
+                    <li><a href="javascript:;">Doação legal</a></li>
+                    <li><a href="javascript:;">Alugue o auditórios</a></li>
+                    <li><a href="javascript:;">Seja um voluntário</a></li>
+                    <li><a href="javascript:;">Doação de cupom fiscal</a></li>
+                    <li><a href="javascript:;">Compre nossos produtos</a></li>
+                    <li><a href="javascript:;">Faça sua doação</a></li>
+                    <li><a href="javascript:;">Outras formas de doação</a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-2 d-none d-md-block">
+                <ul class="menu-footer">
+                    <li class="title-list">Mídia</li>
+                    <li><a href="javascript:;">Notícias</a></li>
+                    <li><a href="javascript:;">Fotos</a></li>
+                    <li><a href="javascript:;"><strong>Diagnóticos precoce</strong></a></li>
+                    <li><a href="javascript:;"><strong>Projetos</strong></a></li>
+                    <li><a href="javascript:;"><strong>Parceiros</strong></a></li>
+                    <li><a href="javascript:;"><strong>Fale conosco</strong></a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="box-newsletter">
+                    <div class="title-newsletter">
+                        <h3><span>Assine nosso</span> informativo</h3>
+                    </div>
+                    <form action="" class="form-newsletter row">
+                        <div class="form-group col-12 col-sm-4">
+                            <input type="text" class="form-control" placeholder="Nome">
+                        </div>
+                        <div class="form-group col-12 col-sm-4">
+                            <input type="text" class="form-control" placeholder="E-mail">
+                        </div>
+                        <div class="form-group col-12 col-sm-4">
+                            <button class="btn-newsletter">Enviar</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="box-adress">
+                    <strong class="title-adress">Endereço</strong>
+                    <a href="javascript:;" class="adress-itself">Rua Clementino Câmara, 234, Barro Vermelho - Natal/RN - CEP 59030-330</a>
+                    <a href="mailto:marketing@casadurvalpaiva.org.br" class="email-adress">marketing@casadurvalpaiva.org.br</a>
+                </div>
+                <div class="box-network">
+                    <ul>
+                        <li><a href="javascript:;"><img src="<?= $this->url('/assets/img/icon-instagram.png') ?>" alt=""></a></li>
+                        <li><a href="javascript:;"><img src="<?= $this->url('/assets/img/icon-instagram.png') ?>" alt=""></a></li>
+                        <li><a href="javascript:;"><img src="<?= $this->url('/assets/img/icon-instagram.png') ?>" alt=""></a></li>
+                        <li><a href="javascript:;"><img src="<?= $this->url('/assets/img/icon-instagram.png') ?>" alt=""></a></li>
+                    </ul>
+                </div>
+                <div class="box-phone">
+                    <a href="javascript" class="phone">
+                        <span class="number-phone">(84) 4006-1600 |</span>
+                        <span class="ligar">ligar</span>
+                    </a>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="box-map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3969.423185983503!2d-35.21014088523295!3d-5.7957484958001295!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b3000f67e4279d%3A0x573543bcfdc0cc0b!2sR.%20Prof.%20Clementino%20C%C3%A2mara%2C%20234%20-%20Barro%20Vermelho%2C%20Natal%20-%20RN%2C%2059030-330!5e0!3m2!1spt-BR!2sbr!4v1570029938117!5m2!1spt-BR!2sbr" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="footer__copyright text-center">
         <a
             class="footer__copyright--link"
